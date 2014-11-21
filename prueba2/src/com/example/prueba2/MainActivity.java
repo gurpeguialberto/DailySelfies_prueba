713cf1e1f8e4441ee3b82fce4cbe4556516a575c
Alberto Gurpegui <gurpegui.alberto@gmail.com>
2014-11-21 19:45:21 +0100

    Added methods to createFile, showSavedFiles... SimpleAdapter.

--- prueba2/src/com/example/prueba2/MainActivity.java
+++ prueba2/src/com/example/prueba2/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.prueba2;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.*;
@@ -15,6 +22,11 @@ public class MainActivity extends Activity {
 	ListView listView ;
 	private static final String TAG = "Photo";
 	static final int REQUEST_IMAGE_CAPTURE = 1;
+	static final int REQUEST_TAKE_PHOTO = 1;
+	String[] SavedFiles;
+	String mCurrentPhotoPath;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,14 +51,15 @@ public class MainActivity extends Activity {
         // Second parameter - Layout for the row
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
-
+/*
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
           android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
 
         // Assign adapter to ListView
         listView.setAdapter(adapter); 
-        
+ */
+        ShowSavedFiles();/* Recalls to ArrayAdapter and sets it */
         // ListView Item Click Listener
         listView.setOnItemClickListener(new OnItemClickListener() {
         	 @Override
@@ -67,6 +80,32 @@ public class MainActivity extends Activity {
               }
         });
     }
+	void ShowSavedFiles(){
+		   SavedFiles = getApplicationContext().fileList();
+		   ArrayAdapter<String> adapter
+		   = new ArrayAdapter<String>(this,
+		     android.R.layout.simple_list_item_1,
+		     SavedFiles);
+
+		   listView.setAdapter(adapter);
+		  }
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
+	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -94,7 +133,23 @@ public class MainActivity extends Activity {
 		if (id == R.id.action_camera) {
 			  Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 			    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-			        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+			    	 // Ensure that there's a camera activity to handle the intent
+			        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			            // Create the File where the photo should go
+			            File photoFile = null;
+			            try {
+			                photoFile = createImageFile();
+			            } catch (IOException ex) {
+			                // Error occurred while creating the File
+			                
+			            }
+			            // Continue only if the File was successfully created
+			            if (photoFile != null) {
+			                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+			                        Uri.fromFile(photoFile));
+			                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			            }
+			        }
 			    }
 			return true;
 		}
